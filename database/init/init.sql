2ff7b3a7f82e5a774383d79ae12133b41d56f5ff
caleb furley <caleb.furley@gmail.com>
2025-09-15 13:26:13 -0500

    Created the database

---
+++ database/init/init.sql
@@ -0,0 +1,8 @@
+-- Basic Test SQL Script to make sure the database works.
+
+CREATE TABLE test (
+    column1 VARCHAR(255),
+    column2 VARCHAR(255)
+);
+
+INSERT INTO test (column1, column2) VALUES ('test1', 'test2');
