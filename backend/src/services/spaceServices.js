a925c18fa2622679b39fe0c09cc3f1b99e62524f
DrewProkarym <drewp2024@gmail.com>
2025-10-01 17:46:50 -0500

    Use DB pool

--- backend/src/services/spaceServices.js
+++ backend/src/services/spaceServices.js
@@ -1,23 +1,37 @@
+const { pool } = require("../db/index.js")
+
 /**
- * Explain function here..
+ * Gets spaced based off userId or the spaceId
  */
-const getSpaces = async (userID, spaceID) => {
-  // get database connection from the pool
+const getSpaces = async (userId, spaceId) => {
+  let queryId;
   let query = "SELECT * FROM space WHERE ";
-  if (userId == NULL && spaceId) {
-    // return a 500 error for internal error
-  } else if (userId.HasValue) {
-    query = query + "created_by_user_id = " + userId;
+
+  // return 400 if bad data, else concat query and find correct id.
+  if (userId == null && spaceId == null) {
+    return { success: false, status: 400, error: "No id provided."}
+  } else if (userId) {
+    query += "created_by_user_id = $1"
+    queryId = userId
   } else {
-    query = query + "id = " + spaceId;
+    query += "id = $1"
+    queryId = spaceId
+  }  
+
+  // returning keyword returns the rows to verify data
+  query += " RETURNING id;"
+  try {
+    const result = pool.query(query, [id])
+    return { success: true, status: 200, data: result.rows[0]}
   }
-  // postgres function to execute on the database
-  let result = databaseResult;
-  return result;
+  catch(error) {
+    console.log(error)
+    return { success: false, status: 500, error: 'Database Error.'}
+  }
 };
 
 const insertSpaceToDatabase = async (space) => {
-  let query = "";
+  let query = 
 };
 
 export default { getSpaces };
