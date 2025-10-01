847a068b5d50bdb95b4aef58fa9eec972a984e52
DrewProkarym <drewp2024@gmail.com>
2025-10-01 17:25:36 -0500

    Create db pool

---
+++ backend/src/db/index.js
@@ -0,0 +1,10 @@
+const { Pool } = require('pg')
+
+const pool = new Pool({
+    connectionString = process.env.DATABASE_STRING,
+    max: 20,
+    idleTimeoutMillis = 30000,
+    connectionTimeoutMillis = 2000
+})
+
+module.exports = pool
