950242f5b9be73e731a77285772d278d8dbaf63b
DrewProkarym <drewp2024@gmail.com>
2025-10-01 20:00:30 -0500

    insertSpacetoDatabase done

--- backend/src/services/spaceServices.js
+++ backend/src/services/spaceServices.js
@@ -1,4 +1,6 @@
-const { pool } = require("../db/index.js")
+const { pool } = require("../db/index.js");
+import spaceModel from "../models/spaceModel.js";
+const Space = require("../models/spaceModel.js");
 
 /**
  * Gets spaced based off userId or the spaceId
@@ -9,45 +11,67 @@ const getSpaces = async (userId, spaceId) => {
 
   // return 400 if bad data, else concat query and find correct id.
   if (userId == null && spaceId == null) {
-    return { success: false, status: 400, error: "No id provided."}
+    return { success: false, status: 400, error: "No id provided." };
   } else if (userId) {
-    query += "created_by_user_id = $1"
-    queryId = userId
+    query += "created_by_user_id = $1";
+    queryId = userId;
   } else {
-    query += "id = $1"
-    queryId = spaceId
-  }  
+    query += "id = $1";
+    queryId = spaceId;
+  }
 
   try {
-    const result = await pool.query(query, [id])
-    if (result.rows.length === 0) { // No results
-      return { success: false, status: 404, error: "No spaces found."}
+    const result = await pool.query(query, [id]);
+    if (result.rows.length === 0) {
+      // No results
+      return { success: false, status: 404, error: "No spaces found." };
+    } else {
+      // Has results
+      return { success: true, status: 200, data: result.rows };
     }
-    else {  // Has results
-      return { success: true, status: 200, data: result.rows }
-    }
+  } catch (error) {
+    console.log(error);
+    return { success: false, status: 500, error: "Database Error." };
   }
-  catch(error) {
-    console.log(error)
-    return { success: false, status: 500, error: 'Database Error.'}
-  }
-  
- 
 };
 
 const insertSpaceToDatabase = async (space) => {
-  let query = 
+  if (space === null || space === undefined) {
+    return { success: false, status: 400, error: "Space is not given." };
+  }
+
+  const query = `
+    INSERT INTO space (created_by_user_id, title, icon,
+    create_date_utc, update_date_utc, delete_date_utc, deleted)
+    VALUES ($1, $2, $3, $4, $5, $6, $7)
+    RETURNING id
+  `;
+
+  values = [
+    space.created_by_user_id,
+    space.title,
+    space.icon ?? null,
+    space.create_date_utc ? new Date(space.create_date_utc) : new Date(),
+    null,
+    null,
+    0,
+  ];
+  try {
+    const result = await pool.query(query);
+    if (result.rows.length === 0) {
+      console.log("Space did not insert:", values);
+      return { success: false, status: 500, error: "Space did not insert" };
+    } else {
+      return {
+        success: true,
+        status: 200,
+        message: `Created space ${result.rows[0].id} succesfully`,
+      };
+    }
+  } catch (error) {
+    console.log(error);
+    return { success: false, status: 500, error: "Server Error" };
+  }
 };
 
-export default { getSpaces };
-
- // // returning keyword returns the rows to verify data
-  // query += " RETURNING id;"
-  // try {
-  //   const result = pool.query(query, [id])
-  //   return { success: true, status: 200, data: result.rows[0]}
-  // }
-  // catch(error) {
-  //   console.log(error)
-  //   return { success: false, status: 500, error: 'Database Error.'}
-  // }
+export default { getSpaces, insertSpaceToDatabase };
