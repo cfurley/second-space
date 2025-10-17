2f343b7b2fe90cb7ede483cdb46fb653bb71049e
DrewProkarym <drewp2024@gmail.com>
2025-10-17 12:15:39 -0500

    Added username validation

---
+++ backend/user_unittest.js
@@ -0,0 +1,16 @@
+import userModel from "/src/models/userModel.js";
+import validatePassword from "/src/controllers/userControllers.js";
+
+// Response object mock
+const createResMock = () => ({
+  status: jest.fn().mockReturnThis(),
+  json: jest.fn(),
+});
+
+describe("Username validation incorrect type int", () => {
+    test("Valid: username was rejected", () => {
+
+        username = 
+
+  });
+});
